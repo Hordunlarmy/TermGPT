7458a5e7061f30be61b10a5bd277d7cf246b3468
hordunlarmy <hordunlarmy@gmail.com>
2023-12-10 08:54:16 +0000

    Refactor code to handle file input and improve user input handling

--- termgpt.py
+++ termgpt.py
@@ -6,23 +6,29 @@ import time
 import readline
 from pygments import highlight
 from pygments.lexers import guess_lexer
+from pygments.lexers import get_lexer_by_name
 from pygments.formatters import TerminalFormatter
-from pygments.token import (Token, Comment, Keyword, Name, String, Number)
+from pygments.token import (Keyword, Name, Comment, String, Error,
+                            Number, Operator, Generic, Token, Whitespace)
+from pygments import highlight
 import colorama
+
 
 # custom color scheme
 COLOR_SCHEME = {
-    Token: ('gray', 'gray'),
-    Comment: ('magenta', 'brightmagenta'),
-    Keyword: ('blue', '**'),
-    Keyword.Type: ('green', '*brightgreen*'),
-    Name.Builtin: ('cyan', 'brightblue'),
-    Name.Function: ('blue', 'brightblue'),
-    Name.Class: ('_green_', 'brightblue'),
-    Name.Decorator: ('magenta', 'brightmagenta'),
-    Name.Variable: ('blue', 'brightblue'),
-    String: ('yellow', 'brightyellow'),
-    Number: ('blue', 'brightyellow')
+    Token:              ('gray',                 'gray'),
+    Comment:            ('magenta',     'brightmagenta'),
+    Comment.Preproc:    ('magenta',     'brightmagenta'),
+    Keyword:            ('blue',                   '**'),
+    Keyword.Type:       ('green',       '*brightgreen*'),
+    Operator.Word:      ('**',                     '**'),
+    Name.Builtin:       ('cyan',           'brightblue'),
+    Name.Function:      ('blue',           'brightblue'),
+    Name.Class:         ('_green_',        'brightblue'),
+    Name.Decorator:     ('magenta',     'brightmagenta'),
+    Name.Variable:      ('blue',           'brightblue'),
+    String:             ('yellow',       'brightyellow'),
+    Number:             ('blue',         'brightyellow')
 }
 
 # Init colors
@@ -32,9 +38,9 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 # Check if the OpenAI API key is present
 if not openai.api_key:
-    print("OpenAI API key not found. "
-          "Please set the OPENAI_API_KEY environment variable.")
-    sys.exit()
+    print("OpenAI API key not found. Please set the"
+          "OPENAI_API_KEY environment variable.")
+    exit()
 
 
 def colorize_code(code):
@@ -52,39 +58,24 @@ def emulate_typing(message):
     print(flush=True)
 
 
-def process_file(user_input):
-    """Process content with a file"""
-    if isinstance(user_input, list):
-        user_input = ' '.join(user_input)
-
-    file_mentions = [word for word in user_input.split()
-                     if os.path.isfile(word)]
-    if file_mentions:
-        for file_mention in file_mentions:
-            with open(file_mention, 'r') as file:
-                file_content = file.read()
-                user_input = user_input.replace(
-                    file_mention, file_content).strip()
-    return user_input
+def process_file(file_path):
+    """Process content from a file"""
+    with open(file_path, 'r') as file:
+        return file.read()
 
 
-def print_chatgpt_response(answer):
-    """Print ChatGPT answer"""
-    print(colorama.Fore.YELLOW + 'ChatGPT: ' +
-          colorama.Style.RESET_ALL, end='')
-    parse_answer = answer.split('```')
-    if len(parse_answer) > 1:
-        emulate_typing(parse_answer[0].strip()[:-1])
-        for chunk in parse_answer[1:]:
-            if chunk.strip():
-                code = '\n'.join(chunk.split('\n')[1:])
-                highlight_code = colorize_code(code)
-                if code.strip():
-                    emulate_typing(highlight_code.strip())
-            else:
-                emulate_typing('\n' + chunk.strip())
-    else:
-        emulate_typing(answer.strip())
+def get_multiline_input():
+    # Read the input until the Enter key is pressed
+    user_input = ''
+    while True:
+        try:
+            char = input()
+            if char == 'quit' or char == 'exit':
+                sys.exit()
+            user_input += char
+        except EOFError:
+            break
+    return user_input.strip()
 
 
 messages = [{
@@ -92,67 +83,80 @@ messages = [{
     'content': 'You are a helpful assistant.'
 }]
 
-if __name__ == "__main__":
-    # Main loop
-    file_processed = False
+# Main loop
+file_processed = False
 
-    while True:
-        try:
-            # non-interactive mode
-            if not file_processed and len(sys.argv) > 1:
-                question = process_file(sys.argv[1:])
-                file_processed = True
+while True:
+    try:
+        if not file_processed and len(sys.argv) > 1:
+            notes, file, *_ = sys.argv[1:]
+            processed_file = process_file(file)
+            question = f"{notes}\n{processed_file}"
+            file_processed = True
+        else:
+            # Get user input
+            sys.stdout.write(colorama.Fore.CYAN + 'You: ' +
+                             colorama.Style.RESET_ALL)
+            sys.stdout.flush()
+            user_input = get_multiline_input()
+
+            # Check if there is a file mentioned in the user input
+            file_mentions = [
+                word for word in user_input.split() if os.path.isfile(word)]
+            if file_mentions:
+                for file_mention in file_mentions:
+                    with open(file_mention, 'r') as file:
+                        file_content = file.read()
+                        user_input = user_input.replace(
+                            file_mention, file_content).strip()
+                question = user_input
             else:
-                # Get user input in interactive mode
-                user_input = input(colorama.Fore.CYAN +
-                                   'You: ' + colorama.Style.RESET_ALL)
-                question = process_file(user_input)
+                question = user_input
 
-            # Check for exit command
-            if question.lower() == 'quit' or question.lower() == 'exit':
-                break
+        # Add user message to the conversation
+        messages.append({
+            'role': 'user',
+            'content': question
+        })
 
-            # Add user message to the conversation
-            messages.append({
-                'role': 'user',
-                'content': question
-            })
+        # OpenAI API response
+        response = openai.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=messages
+        )
 
-            # OpenAI API response
-            response = openai.chat.completions.create(
-                model="gpt-3.5-turbo",
-                messages=messages
-            )
+        # Extract text from response
+        answer = response.choices[0].message.content
 
-            # Extract text from response
-            answer = response.choices[0].message.content
+        # Add ChatGPT message to the conversation
+        messages.append({
+            'role': 'assistant',
+            'content': answer
+        })
 
-            # Add ChatGPT message to the conversation
-            messages.append({
-                'role': 'assistant',
-                'content': answer
-            })
+        # Print ChatGPT answer
+        print(colorama.Fore.YELLOW + 'ChatGPT: ' +
+              colorama.Style.RESET_ALL, end='')
+        parse_answer = answer.split('```')
+        if len(parse_answer) > 1:
+            emulate_typing(parse_answer[0].strip()[:-1])
+            for chunk in parse_answer[1:]:
+                if chunk.strip():
+                    code = '\n'.join(chunk.split('\n')[1:])
+                    highlight_code = colorize_code(code)
+                    if code.strip():
+                        emulate_typing(highlight_code.strip())
+                else:
+                    emulate_typing('\n' + chunk.strip())
+        else:
+            emulate_typing(answer.strip())
 
-            print_chatgpt_response(answer)
+    except KeyboardInterrupt:
+        # Handle Ctrl+C or other interrupt signals
+        print("\nExiting...")
+        break
 
-        # Catch OpenAI API errors
-        except openai.error.RateLimitError as e:
-            print(colorama.Fore.YELLOW +
-                  f"ChatGPT: Rate limit reached - {e}" +
-                  colorama.Style.RESET_ALL)
-            print("Please try again in 20s.")
-            time.sleep(20)
-            break
-
-        except openai.error.OpenAIError as e:
-            print(colorama.Fore.YELLOW +
-                  f"ChatGPT: An error occurred - {e}" +
-                  colorama.Style.RESET_ALL)
-
-        except Exception as e:
-            print(
-                colorama.Fore.YELLOW + "ChatGPT: I'm busy, only 3 requests "
-                "per minute are allowed for free account, please ask your "
-                "question again in 20 seconds" + colorama.Style.RESET_ALL)
-            time.sleep(20)
-            break
+    # Handle other exceptions
+    except Exception as e:
+        print(colorama.Fore.YELLOW +
+              f"ChatGPT: An error occurred - {e}" + colorama.Style.RESET_ALL)
